15453a29c7901eba59ef05bc5d0467c631670690
dlijp <504136021@qq.com>
2020-03-20 09:31:16 +0800

    Import old battle and modify icon

--- src/app/constants/NetworkConst.lua
+++ src/app/constants/NetworkConst.lua
@@ -9,6 +9,6 @@ NetworkConst.TEST1_GATE_PORT = 5860
 NetworkConst.TEST2_GATE_HOST = "192.168.199.189"
 NetworkConst.TEST2_GATE_PORT = 5860
 
-NetworkConst.REMOTE_VERSION_URL = "http://192.168.199.18:8080/examples/servlets/update/version/"
+NetworkConst.REMOTE_VERSION_URL = "http://192.168.199.22:8080/examples/servlets/update/version/"
 
 return NetworkConst
